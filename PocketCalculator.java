cf208e2323ae8c9df4ba693f77c489d73a6b718b
lanature89 <30279856+lanature89@users.noreply.github.com>
2018-05-08 11:12:30 -0400

    Update PocketCalculator.java

--- PocketCalculator.java
+++ PocketCalculator.java
@@ -1,10 +1,7 @@
 /*	Author: Alexis Dionne
- * 	This program acts as a calculator
- * 	Taking 2 inputs from user and outputting
- * 	The results
- * 
+ * 	This program acts as a pocket calculator
  * 	Started: 07/05/2018
- * 	Updated: 07/05/2018
+ * 	Updated: 08/05/2018
  */
 
 import java.util.Scanner;
@@ -13,53 +10,46 @@ public class PocketCalculator {
 
 	public static void main(String[] args) {
 		// MY VARIABLES
-		Scanner operand1 = new Scanner(System.in);
-		Scanner operand2 = new Scanner(System.in);
-		Scanner operand3 = new Scanner(System.in);
-		Scanner operand4 = new Scanner(System.in);
-		int inputOfOperand1, inputOfOperand2;
-		double inputOfOperand3, inputOfOperand4;
-		
+		Scanner userInput = new Scanner(System.in);
+		int number1, number2;
+		double double1, double2;	
+
 		// Step 1, greet the user
 		System.out.println("Hello, thanks for using Pocket Calculator!");
-		
-		// Step 2, ask the users for two operands and assign them to int
+				
+		// Step 2, ask the users for two int and assign them to variables
 		System.out.print("Please enter operand 1: ");
-		inputOfOperand1 = operand1.nextInt();
+		number1 = userInput.nextInt();
 		System.out.print("Please enter operand 2: ");
-		inputOfOperand2 = operand2.nextInt();
+		number2 = userInput.nextInt();
 		
 		// Step 3, calculates the results and print them for int
 		// + , - , * , / , % in that order.
-		System.out.println(inputOfOperand1 + "+" + inputOfOperand2 + "=" + (inputOfOperand1+inputOfOperand2));
-		System.out.println(inputOfOperand1 + "-" + inputOfOperand2 + "=" + (inputOfOperand1-inputOfOperand2));
-		System.out.println(inputOfOperand1 + "*" + inputOfOperand2 + "=" + (inputOfOperand1*inputOfOperand2));
-		System.out.println(inputOfOperand1 + "/" + inputOfOperand2 + "=" + (inputOfOperand1/inputOfOperand2));
-		System.out.println(inputOfOperand1 + "%" + inputOfOperand2 + "=" + (inputOfOperand1%inputOfOperand2));
+		System.out.println(number1 + "+" + number2 + "=" + (number1+number2));
+		System.out.println(number1 + "-" + number2 + "=" + (number1-number2));
+		System.out.println(number1 + "*" + number2 + "=" + (number1*number2));
+		System.out.println(number1 + "/" + number2 + "=" + (number1/number2));
+		System.out.println(number1 + "%" + number2 + "=" + (number1%number2));
 		
 		// Step 4 ask the user for two operands and assign them to double.
 		System.out.println("Now let's try with doubles: ");
 		System.out.print("Please enter operand 3: ");
-		inputOfOperand3 = operand1.nextDouble();
+		double1 = userInput.nextDouble();
 		System.out.print("Please enter operand 4: ");
-		inputOfOperand4 = operand2.nextDouble();
+		double2 = userInput.nextDouble();
 		
 		// Step 5, calculates the results and print them for doubles.
 		// + , - , * , / , % in that order.
-		System.out.println(inputOfOperand3 + "+" + inputOfOperand4 + "=" + (inputOfOperand3+inputOfOperand4));
-		System.out.println(inputOfOperand3 + "-" + inputOfOperand4 + "=" + (inputOfOperand3-inputOfOperand4));
-		System.out.println(inputOfOperand3 + "*" + inputOfOperand4 + "=" + (inputOfOperand3*inputOfOperand4));
-		System.out.println(inputOfOperand3 + "/" + inputOfOperand4 + "=" + (inputOfOperand3/inputOfOperand4));
-		System.out.println(inputOfOperand3 + "%" + inputOfOperand4 + "=" + (inputOfOperand3%inputOfOperand4));
+		System.out.println(double1 + "+" + double2 + "=" + (double1+double2));
+		System.out.println(double1 + "-" + double2 + "=" + (double1-double2));
+		System.out.println(double1 + "*" + double2 + "=" + (double1*double2));
+		System.out.println(double1 + "/" + double2 + "=" + (double1/double2));
+		System.out.println(double1 + "%" + double2 + "=" + (double1%double2));
 		
 		
 		// Step 6, close the scanner and say goodbye to user!
-		operand1.close();
-		operand2.close();
-		operand3.close();
-		operand4.close();
+		userInput.close();
 		System.out.println("Thank you for trusting PocketCalculator with your precious numbers.");
-		
 	}
 
 }
